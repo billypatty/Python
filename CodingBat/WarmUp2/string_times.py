c2b3014426e5895d13fef5d1375f83eaa10e2eb1
billypatty <148037151+billypatty@users.noreply.github.com>
2024-06-24 20:16:19 +0300

    Update string_times.py

--- CodingBat/WarmUp2/string_times.py
+++ CodingBat/WarmUp2/string_times.py
@@ -1,1 +1,10 @@
+"""
+question:
 
+Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+my solution:
+"""
+
+def string_times(str, n):
+  return str*int(n)
