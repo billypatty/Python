8aad946885b2f0e4de64a8fedc2b1f7a602bb650
billypatty <148037151+billypatty@users.noreply.github.com>
2024-06-24 19:15:56 +0300

    Create not_string.py

---
+++ CodingBat/WarmUp1/not_string.py
@@ -0,0 +1,15 @@
+"""
+question:
+
+Given a string, return a new string where "not " has been added to the front. 
+However, if the string already begins with "not", return the string unchanged. 
+
+my solution:
+"""
+
+def not_string(str):
+  k = str.split(' ') # divide the given string from its spaces
+  if k[0] == 'not': # if the first element is 'not'
+    return str
+  else:
+    return 'not ' + str # return 'not' and the string after it, making the first word 'not'.
