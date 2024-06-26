bee3418ddc2a1a67a4e3e915a1214acc914443f1
billypatty <148037151+billypatty@users.noreply.github.com>
2024-06-26 17:16:12 +0300

    Create last2.py

---
+++ CodingBat/WarmUp2/last2.py
@@ -0,0 +1,11 @@
+"""
+question:
+
+Given a string, return the count of the number of times that a substring length 2 appears in the string 
+and also as the last 2 chars of the string,
+so "hixxxhi" yields 1 (we won't count the end substring).
+
+my solution:
+we're trying to find how many times the last two letters of a given word appears in the word
+"""
+
