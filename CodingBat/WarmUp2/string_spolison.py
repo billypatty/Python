c93aecc4042535a3222b6445a45ae876f4445dae
billypatty <148037151+billypatty@users.noreply.github.com>
2024-06-27 10:24:42 +0300

    Update indent of string_spolison.py

--- CodingBat/WarmUp2/string_spolison.py
+++ CodingBat/WarmUp2/string_spolison.py
@@ -7,9 +7,9 @@ my solution:
 """
 
 def string_splosion(s):
-  a = '' # an empty string
-  for i in range(len(s)): # repeat it for every leter in the word 's',
-    for j in range(0,i): # from the first letter, go until the ith letter,
-      a += s[j] # add it to the initially empty string 'a'
-  a += s # also add the word itself, since range function doesn't cover all
-  return a
+    a = '' # an empty string
+    for i in range(len(s)): # repeat it for every leter in the word 's',
+        for j in range(0,i): # from the first letter, go until the ith letter,
+            a += s[j] # add it to the initially empty string 'a'
+    a += s # also add the word itself, since range function doesn't cover all
+    return a
