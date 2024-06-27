590b523a8b569892d776a5184603f560aca983bb
billypatty <148037151+billypatty@users.noreply.github.com>
2024-06-27 10:23:17 +0300

    Update indent of diff21.py

--- CodingBat/WarmUp1/diff21.py
+++ CodingBat/WarmUp1/diff21.py
@@ -7,7 +7,7 @@ my solution:
 """
 
 def diff21(a): # define a function with a single input, an integer.
-  if a<=21: # if that integer is not greater than 21,
-    return abs(a-21) # use the 'abs' function to return their absolute value
-  else: # other cases 
-    return 2*abs(a-21) # multiply the absolute value by two.
+    if a<=21: # if that integer is not greater than 21,
+        return abs(a-21) # use the 'abs' function to return their absolute value
+    else: # other cases 
+        return 2*abs(a-21) # multiply the absolute value by two.
