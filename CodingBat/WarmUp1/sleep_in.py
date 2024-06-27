a36a71c0cff030f7c3d674d4ea062969e8ab5d80
billypatty <148037151+billypatty@users.noreply.github.com>
2024-06-27 10:11:57 +0300

    Update indent of sleep_in.py

--- CodingBat/WarmUp1/sleep_in.py
+++ CodingBat/WarmUp1/sleep_in.py
@@ -8,7 +8,7 @@ my solution:
 """
 
 def sleep_in(w, v): # w for weekday and v for vacation
-  if not w: # if it is not a weekday,
-    return True # regardless of the value of v, we will sleep in.
-  elif w: # if it is a weekday,
-    return v # if we are on a vacation, we'll sleep in. otherwise, we won't.
+    if not w: # if it is not a weekday,
+        return True # regardless of the value of v, we will sleep in.
+    elif w: # if it is a weekday,
+        return v # if we are on a vacation, we'll sleep in. otherwise, we won't.
