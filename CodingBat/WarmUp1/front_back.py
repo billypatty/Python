6b02d49d4ff5d1dbc4bf3413c05872a8b3230ef0
billypatty <148037151+billypatty@users.noreply.github.com>
2024-06-27 10:22:01 +0300

    Update indent of front_back.py

--- CodingBat/WarmUp1/front_back.py
+++ CodingBat/WarmUp1/front_back.py
@@ -6,13 +6,13 @@ Given a string, return a new string where the first and last chars have been exc
 my solution:
 """
 
-def front_back(str):
-  if len(str) <= 1: # if the string is empty or a single letter only, there is nothing to flip
-    return str
-  else:
-    copstr = '' # an empty string
-    copstr += str[-1] # add to it the last letter
-    for i in range(1, len(str) -1): #skip the first letter & go until the last letter 
-      copstr += str[i] # generate the string using the loop
-    copstr += str[0] # lastly, add the first letter to the string
-    return copstr
+def front_back(s):
+    if len(s) <= 1: # if the string is empty or a single letter only, there is nothing to flip
+        return s
+    else:
+        copstr = '' # an empty string
+        copstr += s[-1] # add to it the last letter
+        for i in range(1, len(s) -1): #skip the first letter & go until the last letter 
+            copstr += s[i] # generate the string using the loop
+        copstr += s[0] # lastly, add the first letter to the string
+        return copstr
