341390c0c8beb98e6d141e39fff5a222f599748e
billypatty <148037151+billypatty@users.noreply.github.com>
2024-06-27 10:25:12 +0300

    Update string_bits.py

--- CodingBat/WarmUp2/string_bits.py
+++ CodingBat/WarmUp2/string_bits.py
@@ -9,8 +9,8 @@ we should be careful about words with even number of letters.
 """
 
 def string_bits(s):
-  a = '' # define an empty string
-  for i in range(len(s)): # for every letter in 's' times,
-    if i%2==0: # (remember that indexing starts at 0)
-      a += s[i] # add it to the initially empty string
-  return a
+    a = '' # define an empty string
+    for i in range(len(s)): # for every letter in 's' times,
+        if i%2==0: # (remember that indexing starts at 0)
+            a += s[i] # add it to the initially empty string
+    return a
