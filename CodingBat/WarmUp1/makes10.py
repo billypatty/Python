2355f15dd95f79a80a6a1d840e04cf7501e82c21
billypatty <148037151+billypatty@users.noreply.github.com>
2024-06-29 21:44:56 +0300

    Update indent and remove comments on makes10.py

--- CodingBat/WarmUp1/makes10.py
+++ CodingBat/WarmUp1/makes10.py
@@ -6,10 +6,10 @@ Given 2 ints, a and b, return True if one if them is 10 or if their sum is 10.
 my solution:
 """
 
-def makes10(a, b): # keep in mind that the variable names used in the function don't matter.
-    if a+b == 10: # if the sum of a and b are equal to ten,
-        return True
-    elif a == 10 or b == 10: # if either is 10,
-        return True
-    else: # other cases
-        return False
+def makes10(a, b): 
+  if a+b == 10: 
+    return True
+  elif a == 10 or b == 10: 
+    return True
+  else: 
+    return False
