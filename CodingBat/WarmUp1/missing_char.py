c04fd2bea4189da049ff6e9257a6fd5e19b3420f
billypatty <148037151+billypatty@users.noreply.github.com>
2024-06-29 21:45:43 +0300

    Update missing_char.py

--- CodingBat/WarmUp1/missing_char.py
+++ CodingBat/WarmUp1/missing_char.py
@@ -7,13 +7,13 @@ The value of n will be a valid index of a char in the original string (i.e. n wi
 my solution:
 """
 
-def missing_char(s, n): # s for the string input, n for the index
-    a = '' # an empty string
-    b=[] # an empty list
-    for i in s: # for every letter in the given input,
-        b.append(i) # add it to the list.
-    c=b[n] # c is equal to what we want to remove
-    b.remove(c) # remove that from the list
-    for j in b: # for every letter in the list,
-        a += str(j) # convert it to string and add it to the initially empty string 'a'
-    return a
+def missing_char(s, n): 
+  a = '' 
+  b=[] 
+  for i in s: 
+    b.append(i) 
+  c=b[n] 
+  b.remove(c) 
+  for j in b:
+    a += str(j)
+  return a
