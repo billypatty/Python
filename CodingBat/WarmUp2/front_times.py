6ed66bdd4e8ce5dbd16f5cefd093d59ce214629e
billypatty <148037151+billypatty@users.noreply.github.com>
2024-06-30 15:26:47 +0300

    Update front_times.py

--- CodingBat/WarmUp2/front_times.py
+++ CodingBat/WarmUp2/front_times.py
@@ -9,10 +9,10 @@ my solution:
 """
 
 def front_times(s, n):
-    if len(s) <= 3: # check if the word is shorter than 3 letters
-        return s*int(n)
-    else: # if it isn't,
-        a = ''
-        for i in range(3):
-            a += s[i]
-        return a*int(n)
+  if len(s) <= 3: 
+    return s*int(n)
+  else: # if it isn't,
+    a = ''
+    for i in range(3):
+      a += s[i]
+    return a*int(n)
