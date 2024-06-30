4773ecba3f4405e6b9e905858ea6cdaa7c4cd24f
billypatty <148037151+billypatty@users.noreply.github.com>
2024-06-30 15:27:58 +0300

    Update last2.py

--- CodingBat/WarmUp2/last2.py
+++ CodingBat/WarmUp2/last2.py
@@ -10,13 +10,13 @@ we're trying to find how many times the last two letters of a given word appears
 """
 
 def last2(s):
-    count = 0
-    if len(s) <= 2: # for strings shorter than 2 chars, there can't be any repetition.
-        return 0
-    else: # for longer strings,
-        for i in range(len(s)-1):
-            if i != len(s):
-                if s[i] == s[-2]:
-                    if s[i+1] == s[-1]:
-                        count +=1
-    return count -1
+  count = 0
+  if len(s) <= 2:  
+    return 0
+  else:  
+    for i in range(len(s)-1):
+      if i != len(s):
+        if s[i] == s[-2]:
+          if s[i+1] == s[-1]:
+            count +=1
+  return count -1
