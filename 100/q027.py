ad0db3ba29963acefd5fd873aa2b578f4cd34c7f
sushismurf <148037151+sushismurf@users.noreply.github.com>
2024-09-02 19:40:24 +0300

    Create q027.py

---
+++ 100/q027.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  2 19:39:04 2024.
+
+q: Define a function that can convert a integer into a string and print it
+in console.
+
+@author: sushismurf
+"""
+
+def int_toStr(a):
+    return str(a)
